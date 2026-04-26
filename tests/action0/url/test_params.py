1fe89dbca9a94f65981686c6897efb3205ee5755
Simon Lachinger <simon@lachinger.org>
2026-04-26 15:49:26 +0200

    add first few tests for testing the action.url.params.Params class

--- tests/action0/url/test_params.py
+++ tests/action0/url/test_params.py
@@ -1,8 +1,61 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from url import Params
 
-if __name__ == '__main__':
+
+class ParamsInitTestCase(unittest.TestCase):
+    """
+    test for :py:meth:`action0.url.params.Params.__init__`
+    """
+
+    def test_separator_init(self):
+        """ """
+        self.assertEqual(Params().separator, "&")
+        self.assertEqual(Params(params=None, separator="&").separator, "&")
+        self.assertEqual(Params(None, "&").separator, "&")
+        self.assertEqual(Params("a=b", "&").separator, "&")
+
+        self.assertEqual(Params(separator=";").separator, ";")
+        self.assertEqual(Params(params=None, separator=";").separator, ";")
+        self.assertEqual(Params(None, ";").separator, ";")
+        self.assertEqual(Params("a=b", ";").separator, ";")
+
+    def test_emtpy_init(self):
+        """
+        Test initialization with no params
+        """
+        params = Params()
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, "&")
+
+    def test_emtpy_init_with_none(self):
+        """
+        Test initialization with `None` for params
+        """
+        params = Params(None)
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, "&")
+
+        params = Params(params=None)
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, "&")
+
+    def test_emtpy_init_with_separator(self):
+        """
+        Test initialization with `None` for params and a separator
+        """
+        params = Params(separator=";")
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, ";")
+
+        params = Params(None, ";")
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, ";")
+
+        params = Params(params=None, separator=";")
+        self.assertEqual(params.as_str(), "")
+        self.assertEqual(params.separator, ";")
+
+
+if __name__ == "__main__":
     unittest.main()
